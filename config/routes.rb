17126879394b10493e3448ca5c10193d6ccf8c4b
Jon Cabot <jfc0053@gmail.com>
2024-10-21 19:43:58 -0500

    added comments model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "articles#index"
 # resources = routes, controller, and views in 1 entity.
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 end
